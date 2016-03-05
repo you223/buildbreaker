8a920b30b88674d3094ae57a0badc2c78f6b7261
wataru <mail2wokada@gmail.com>
2016-03-06 08:54:54 +0900

    add debug print api "/checkResult"

--- web-api/web.go
+++ web-api/web.go
@@ -8,17 +8,34 @@ import (
 	"os"
 )
 
+type State int
+
 const (
-	SUCCEEDED = iota
+	UNKNOWN = iota
+	SUCCEEDED
 	FAILED
 	RECOVERED
 )
 
-var state = SUCCEEDED
+func (s State) String() string {
+	switch s {
+	case SUCCEEDED:
+		return "SUCCEEDED"
+	case FAILED:
+		return "FAILED"
+	case RECOVERED:
+		return "RECOVERED"
+	default:
+		return "Unknown"
+	}
+}
+
+var state State = SUCCEEDED
 var author string
 
 func main() {
 	http.HandleFunc("/putResult", putResult)
+	http.HandleFunc("/checkResult", checkResult)
 	http.HandleFunc("/", hello)
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
@@ -45,5 +62,9 @@ func putResult(res http.ResponseWriter, req *http.Request) {
 	} else {
 		state = FAILED
 	}
+}
 
+func checkResult(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "State=", state.String())
+	fmt.Fprintln(res, "Author=", author)
 }
